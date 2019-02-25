b419c6eeaf958c5a2a35072308fc8a1ca9c71cbe
HopenX <1152214965@qq.com>
2019-02-25 19:49:34 +0800

    继续3n+1猜想
    快速存储的信息，少用链表，多用数组

--- 1005.cpp
+++ 1005.cpp
@@ -1,1 +1,66 @@
+// PTA.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 
+#include "pch.h"
+#include <iostream>
+#include <string> 
+#include<vector>
+#include<list>
+#include<algorithm>
+#include <set>
+#include<iterator>
+#include<cstring>
+using namespace std;
+
+
+int main()
+{
+	int num;
+	int n;
+	int arr[100000];
+	int sign[100000];
+	vector<int> container;
+
+	memset(sign, 0, sizeof(sign));
+	memset(arr, 0, sizeof(arr));
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		arr[i] = num;
+		while (num != 1) {
+			if (num % 2 == 0) {
+				num /= 2;
+				sign[num] = 1;
+				continue;   //注意continue
+			}
+			else {
+				num = (num * 3 + 1)/2;
+				sign[num] = 1;
+				continue;   //注意continue
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (sign[arr[i]] == 0) {    //关键
+			container.push_back(arr[i]);
+		}
+	}
+
+	sort(container.begin(), container.end());
+	vector<int>::reverse_iterator p = container.rbegin();
+	bool isFirst = true;
+	for (; p != container.rend(); p++) {
+		if (isFirst) {
+			cout << *p;
+			isFirst = false;
+		}
+		else
+		{
+			cout << " " << *p;
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
